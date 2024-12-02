51414c5dfd627369d9dd8fd96ebb5f5123639602
cpp-archer <jennalouisa.dja150@gmail.com>
2024-12-02 22:27:51 +0100

    quelques routes
    routes vers les index des dossiers jeux, genres, editeurs grace à leur "creation" dans accueil.hbs en href  (jme comprend)

--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const app = express();
 const PORT = 3005;
 
 
-// app.set("view engine", "hbs"); // config notre moteur de template
-// app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs"); // config notre moteur de template
+app.set("views", path.join(__dirname, "views"));
 const prisma = new PrismaClient();
 
 
 app.get("/", async (req, res) => {
-    //res.render("index");
-    res.send();
+    res.render("accueil");
+    //res.send();
 });
+
+app.get("/Jeux", async (req,res)=> { //on va dans la route /jeux qu'on a creer en tant que href et on recup l'index du doss JEUX
+    res.render("Jeux/index");
+})
+
+app.get("/Genres", async (req,res)=> { //on va dans la route /genre et on recup l'index du doss GENRE
+    res.render("Genres/index");
+})
+
+app.get("/Editeurs", async (req,res)=> { //on va dans la route /editeurs et on recup l'index du doss ED
+    res.render("Editeurs/index");
+})
 
 app.listen(PORT, () => {
     console.log(`Ca marche sur le port ${PORT}`);
