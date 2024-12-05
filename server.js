4f515cb5df5a4692aabb38bcd1a3e32a013bde2e
cpp-archer <jennalouisa.dja150@gmail.com>
2024-12-05 21:23:00 +0100

    alaid..galere sur afficher details d'un jeu
    ajout d'une donnee dans la bdd
    route cree vers le details d'un jeu
    mais arrive pas a afficher (pb avec le unique faut que je change)

--- server.js
+++ server.js
@@ -5,28 +5,43 @@ const path = require("path"); //on utilise path car ca s'adapte a tuot les syste
 const app = express();
 const PORT = 3005;
 
-
-app.set("view engine", "hbs"); // config notre moteur de template
+// config notre moteur de template
+app.set("view engine", "hbs"); 
 app.set("views", path.join(__dirname, "views"));
 const prisma = new PrismaClient();
 
-
+//page d'accueil
 app.get("/", async (req, res) => {
     res.render("accueil");
     createTestDb();
     //res.send();
 });
 
-app.get("/Jeux", async (req,res)=> { //on va dans la route /jeux qu'on a creer en tant que href et on recup l'index du doss JEUX
+//on va dans la route /jeux qu'on a creer en tant que href et on recup l'index du doss JEUX
+app.get("/Jeux", async (req,res)=> { 
     const jeu = await prisma.Game.findMany(); // on va prendre tout les jeux de la table Game
-    res.render("Jeux/index", {jeu}); //on les renvois à index
+    const genre = await prisma.Genre.findMany(); 
+    res.render("Jeux/index", {jeu, genre,}); //on les renvois à index
 })
 
-app.get("/Genres", async (req,res)=> { //on va dans la route /genre et on recup l'index du doss GENRE
+
+//quand on clique sur un jeu, on va sur son detail 
+app.get("/Jeux/:titre/details", async (req,res)=> {
+
+    //je peux pas parce que titre est pas unique... jvais essayer ID 
+    const {titre} = req.params;
+    const jeu = await prisma.Game.findUnique({where: {titre},});
+    res.render("Jeux/DetailsJeux", {jeu});
+})
+
+//on va dans la route /genre et on recup l'index du doss GENRE
+app.get("/Genres", async (req,res)=> { 
+    const genre = await prisma.Genre.findMany();
     res.render("Genres/index");
 })
 
-app.get("/Editeurs", async (req,res)=> { //on va dans la route /editeurs et on recup l'index du doss ED
+ //on va dans la route /editeurs et on recup l'index du doss ED
+app.get("/Editeurs", async (req,res)=> {
     res.render("Editeurs/index");
 })
 
@@ -34,16 +49,20 @@ app.listen(PORT, () => {
     console.log(`Ca marche sur le port ${PORT}`);
 });
 
+
+//test de base de donnée
+//refaire car BORDEL
 async function createTestDb()
 {
     const jeu = {
-        titre: "test",
+        titre: "trr",
         description: "test",
         releaseDate: new Date(2018,22,1),
         genreId: 1,
         editeurId: 1
     };
 
+    
     const genre = {
         nom: "FPS"
 
@@ -54,14 +73,46 @@ async function createTestDb()
 
     };
 
-    // const ed = await prisma.Editeur.create({
-    //     data: editeur, 
-    // });
-    // const gre = await prisma.Genre.create({
-    //     data: genre, 
-    // });
+    const ed = await prisma.Editeur.create({
+        data: editeur, 
+    });
+    const gre = await prisma.Genre.create({
+        data: genre, 
+    });
 
     const je = await prisma.Game.create({
         data: jeu, 
     });
+
+
+//test d'une deuxieme donnee pour voir si quand on clique les details sont bien affiliés au bon jeu
+    const deux= {
+        titre: "NOP",
+        description: "NOP",
+        releaseDate: new Date(2019,12,5),
+        genreId: 2,
+        editeurId: 2
+    };
+
+    const genre2 = {
+        nom: "MMO"
+
+    };
+
+    const editeur2 = {
+        nom: "NOOOO"
+
+    };
+
+    const ed2 = await prisma.Editeur.create({
+        data: editeur2, 
+    });
+    const gre2 = await prisma.Genre.create({
+        data: genre2, 
+    });
+
+    const je2 = await prisma.Game.create({
+        data: deux, 
+    });
+
 }
